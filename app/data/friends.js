eacb027ba932d5a05397ee90bd2e21f0e25c2e86
sdmay10 <sdmay10@gmail.com>
2016-12-19 15:54:25 -0500

    Functional

--- app/data/friends.js
+++ app/data/friends.js
@@ -32,6 +32,22 @@ var friends = [
     ]
 },
 {
+    "name": "Charles",
+    "photo": "http://4.bp.blogspot.com/-C60-Zp3UANY/Uucb8RbouSI/AAAAAAAAAMk/zpPrU6yNQRQ/s1600/1-26-2014+001e.jpg",
+    "scores": [
+          1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     5
+    ]
+},
+{
     "name": "Bill",
     "photo": "http://josephcphillips.com/wp-content/uploads/2015/07/cosby2-620x330.jpg",
     "scores": [
@@ -46,8 +62,88 @@ var friends = [
      5,
      5
     ]
+},
+{
+    "name": "Kanye",
+    "photo": "https://d235mwrq2dn9n5.cloudfront.net/wp-content/uploads/2016/11/rtr_kanye_west_jc_150407_16x9_992-1.jpg",
+    "scores": [
+          3,
+     3,
+     3,
+     3,
+     3,
+     3,
+     3,
+     3,
+     3,
+     3
+    ]
+},
+{
+    "name": "Kim",
+    "photo": "http://cdn.bearingarms.com/uploads/2016/10/481868694_kim-kardashian-zoom-10e8fb5f-d072-4bff-b89a-81b5bc01ae89-1.jpg",
+    "scores": [
+          4,
+     4,
+     4,
+     4,
+     4,
+     4,
+     4,
+     4,
+     4,
+     4
+    ]
+},
+{
+    "name": "Tom",
+    "photo": "http://sports.cbsimg.net/images/blogs/Tom-Brady-ten-years-10-21-15.jpg",
+    "scores": [
+          2,
+     2,
+     2,
+     2,
+     2,
+     2,
+     2,
+     2,
+     2,
+     2
+    ]
+},
+{
+    "name": "Bruce",
+    "photo": "https://pmcdeadline2.files.wordpress.com/2014/02/bruce_willis__140205103344.jpg",
+    "scores": [
+          1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1,
+     1
+    ]
+},
+{
+    "name": "Janeane",
+    "photo": "http://1.images.comedycentral.com/images/ccstandup/comedians/800x600/JaneaneGarofalo_800x600.jpg?width=800&height=600&crop=true&quality=0.91",
+    "scores": [
+          5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5,
+     5
+    ]
 }
+
 ]
 
-// Note how we export the array. This makes it accessible to other files using require.
 module.exports = friends;
