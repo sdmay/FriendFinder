d230dea052e4ad1f8bc9a5b29d3a683a3d00628c
sdmay10 <sdmay10@gmail.com>
2016-12-19 10:33:17 -0500

    First commit

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+{
+  "name":"Ken",
+  "photo":"https://i.ytimg.com/vi/hVQgp9Z6sIg/maxresdefault.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     2,
+     3,
+     4,
+     3,
+     4   
+      ]
+},
+{
+    "name": "Steve",
+    "photo": "http://a5.files.biography.com/image/upload/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE1ODA0OTcxODI5MzMxNDY5.jpg",
+    "scores": [
+          5,
+     1,
+     4,
+     3,
+     2,
+     5,
+     4,
+     3,
+     4,
+     5
+    ]
+},
+{
+    "name": "Bill",
+    "photo": "http://josephcphillips.com/wp-content/uploads/2015/07/cosby2-620x330.jpg",
+    "scores": [
+          5,
+     1,
+     1,
+     3,
+     2,
+     1,
+     4,
+     3,
+     5,
+     5
+    ]
+}
+]
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+// app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static('app'));
+
+
+
+
+require('./app/routing/api-route.js')(app); 
+require('./app/routing/html-routes.js')(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
