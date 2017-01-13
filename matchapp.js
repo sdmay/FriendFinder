b9b2528caf6be7a89caeeba6bbcf0ce55f21e667
sdmay10 <sdmay10@gmail.com>
2017-01-13 09:35:37 -0500

    Added PORT

--- matchapp.js
+++ matchapp.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
-var PORT = 3000;
+var PORT = proces.env.PORT || 3000
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -34,6 +34,6 @@ app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
+// app.listen(PORT, function() {
+//   console.log("App listening on PORT " + PORT);
+// });
